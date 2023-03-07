661fc6f1b81f1c321b2fae36d79c4f3c54fc1597
Bryan Chiam <chibj006@mymail.unisa.edu.au>
2023-03-07 18:28:35 +1030

    basic greetings and start of account number enter

---
+++ ATM machine.py
@@ -0,0 +1,31 @@
+### ATM Machine Simulator Program ###
+## Created by Bryan Chiam ##
+
+# function for greeting and getting the program started 
+def greetings():
+    print("***Welcome to Our One Stop ATM Machine!***\n")
+    access = input("Please press a random number to continue. ")
+    while access.isalpha() : 
+        print('')
+        print("Invalid character typed!")
+        access = input("Please try again, enter ONLY numbers!\n ")
+    return True
+
+greetings()
+
+accNumb = int(input("Please enter your account number below. "))
+emptySymbol = []
+for index in emptySymbol:
+    index < 6
+    emptySymbol[index] = accNumb
+    emptySymbol.append = accNumb
+    index += 1
+print(emptySymbol)
+    
+
+
+
+
+
+
+
